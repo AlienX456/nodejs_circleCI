e0857c1d9d857af1b14fce284909906bd9eee6c4
alienx456 <estebanelias27@gmail.com>
2020-06-22 12:17:01 -0500

    Special ops added and tested

---
+++ special_ops.js
@@ -0,0 +1,14 @@
+const abs = (num)  => {return Math.abs(num)}
+
+const ceil = (num)  => {return Math.ceil(num)}
+
+const floor = (num)  => {return Math.floor(num)}
+
+const round = (num)  => {return Math.round(num)}
+
+module.exports = {
+    abs,
+    ceil,
+    floor,
+    round
+}
